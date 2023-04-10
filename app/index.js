3c9d95070cd4326f219324cca9f6e839d8f6a9e5
aipilotbyjd <128741012+aipilotbyjd@users.noreply.github.com>
2023-04-10 16:09:39 +0000

    dqd

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BussnessNameGenrator = () => {
+    return (
+        <View>
+            <Text>BussnessNameGenrator</Text>
+        </View>
+    )
+}
+
+export default BussnessNameGenrator
+
+const styles = StyleSheet.create({})
